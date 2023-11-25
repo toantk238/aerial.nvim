d91d69f88c3a5d495b81be8e4a16940590676152
rock-tran <rock.tran@manadr.com>
2023-11-26 01:47:57 +0700

    Add kotlin

---
+++ queries/kotlin/aerial.scm
@@ -0,0 +1,9 @@
+(class_declaration
+  name: (type_identifier) @name
+  (#set! "kind" "Class")
+  ) @symbol
+
+(function_declaration
+  name: (simple_identifier) @name
+  (#set! "kind" "Method")
+  ) @symbol
