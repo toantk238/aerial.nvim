7dfe9c1706c843f7882afe0e1182be540c3d1dcb
rock-tran <rock.tran@manadr.com>
2023-11-27 00:47:39 +0700

    Update kotlin

--- queries/kotlin/aerial.scm
+++ queries/kotlin/aerial.scm
@@ -7,3 +7,12 @@
   name: (simple_identifier) @name
   (#set! "kind" "Method")
   ) @symbol
+
+(class_declaration
+  body: (class_body
+    (property_declaration
+      variable: (variable_declaration) @name @symbol
+    )
+  )
+  (#set! "kind" "Property")
+)
